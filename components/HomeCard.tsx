fca1eb4f6f8a5ec8c06435d7fcc911b16ebd1f94
ajgithub394 <123960210+ajgithub394@users.noreply.github.com>
2024-12-19 16:57:59 +0530

    Update HomeCard.tsx

--- components/HomeCard.tsx
+++ components/HomeCard.tsx
@@ -10,7 +10,7 @@ interface HomeCardProps{
     handleClick : ()=> void;
 }
 
-const HomeCard = ({className,img,title,description,handleClick} : HomeCardProps) => {
+export default function HomeCard = ({className,img,title,description,handleClick} : HomeCardProps) => {
   return (
     <div className={cn('bg-orange-1 px-4 py-6 flex flex-col justify-between w-full xl:max-w-[270px] min-h-[260px] rounded-[14px] cursor-pointer',className)} onClick={handleClick}>
         <div className='flex-center glassmorphism size-12 rounded-[10px]'>
@@ -24,5 +24,3 @@ const HomeCard = ({className,img,title,description,handleClick} : HomeCardProps)
     </div>
   )
 }
-
-export default HomeCard
