86ecfd9a051bcf15487da4e3a85c447723d269ed
ajgithub394 <123960210+ajgithub394@users.noreply.github.com>
2024-12-19 16:50:31 +0530

    Update page.tsx

--- app/(root)/(home)/previous/page.tsx
+++ app/(root)/(home)/previous/page.tsx
@@ -1,7 +1,7 @@
 import CallList from '@/components/CallList'
 import React from 'react'
 
-const Previous = () => {
+export default function Previous = () => {
   return(
     <section className='flex size-full flex-col gap-10 text-white'>
       <h1 className='text-3xl font-bold'>
@@ -13,5 +13,3 @@ const Previous = () => {
     </section>
   )
 }
-
-export default Previous
