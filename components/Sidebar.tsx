e1dd708ce1f3af36cb60631c017966df1c471e2b
ajgithub394 <123960210+ajgithub394@users.noreply.github.com>
2024-12-20 12:04:33 +0530

    Update Sidebar.tsx

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -7,7 +7,7 @@ import Link from 'next/link';
 import { usePathname } from 'next/navigation'
 import React from 'react'
 
-export default function Sidebar() => {
+export default function Sidebar(){
   const pathname = usePathname();
   return (
     <section className='sticky left-0 top-0 flex h-screen w-fit flex-col justify-between bg-dark-1 p-6 pt-28 text-white max-sm:hidden lg:w-[264px]'>
