444070794691f87a32319242cbfefb052cd98531
ajgithub394 <123960210+ajgithub394@users.noreply.github.com>
2024-12-20 11:38:00 +0530

    Update page.tsx

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,7 +1,7 @@
 import { SignIn } from '@clerk/nextjs'
 import React from 'react'
 
-export default function SignInPage = () => {
+export default function SignInPage() => {
   return (
     <main className='flex h-screen w-full items-center justify-center'>
         <SignIn/>
