896b1910ad2b64646361a8da70ad229b22fb971c
ajgithub394 <123960210+ajgithub394@users.noreply.github.com>
2024-12-20 11:58:51 +0530

    Update page.tsx

--- app/(root)/(home)/recordings/page.tsx
+++ app/(root)/(home)/recordings/page.tsx
@@ -1,7 +1,7 @@
 import CallList from '@/components/CallList'
 import React from 'react'
 
-export default function Recordings() => {
+export default function Recordings(){
   return(
     <section className='flex size-full flex-col gap-10 text-white'>
       <h1 className='text-3xl font-bold'>
