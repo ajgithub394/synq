d713f004f7a67fadf904f2d47c987492427d6260
ajgithub394 <123960210+ajgithub394@users.noreply.github.com>
2024-12-20 11:58:15 +0530

    Update page.tsx

--- app/(root)/(home)/personal-room/page.tsx
+++ app/(root)/(home)/personal-room/page.tsx
@@ -16,7 +16,7 @@ const Table = ({title,description} : {title : string ; description : string;}) =
   </div>
 )
 
-export default function PersonalRoom() => {
+export default function PersonalRoom(){
   const {user} = useUser();
   const meetingId = user?.id;
   const {toast} = useToast();
